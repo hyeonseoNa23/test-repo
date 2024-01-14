aff1aaa821d63dce3f7642e78c83b8bf21ba6b7e
hyeonseoNa23 <133120043+hyeonseoNa23@users.noreply.github.com>
2024-01-14 14:14:28 +0900

    forth commit
    코드를 작성했습니다.

--- github_test.ino
+++ github_test.ino
@@ -2,11 +2,24 @@ hello world
 /*안녕 세계*/
 
 void setup() {
-
-  
+  pinMode(2,INPUT);
+  pinMode(3,INPUT);
+  pinMode(9,OUTPUT);
 }
 
 viod loop() {
+  int btn_H = digitalRead(2);
+  int btn_L = digitalRead(3);
 
-  
+  if(btn_H == HIGH) {
+    analogWrite(9,255);
+  }
+
+  else if (btn_L == HIGH) {
+    analodgWrite(9,127);
+  }
+
+  else{
+    digitalWrite(9,0);
+  }
 }
